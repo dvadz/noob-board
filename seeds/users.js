9d429d78bb881b0347c45c2fd2db9ff410bbf57a
Daniel Vadil <dvadz@yahoo.com>
2022-06-01 17:26:15 -0400

    Created a list of users for seeding

---
+++ seeds/users.js
@@ -0,0 +1,25 @@
+const users = [
+  {
+    email: "pedestrianrunner-Mary@yahoo.com",
+    password: "daniel",
+    userName: "Daniel",
+    firstName: "Daniel",
+    lastName: "Daniel",
+  },
+  {
+    email: "pedestrianrunner-john@yahoo.com",
+    password: "johnjohn",
+    userName: "John",
+    firstName: "John",
+    lastName: "John",
+  },
+  {
+    email: "pedestrianrunner-mary@yahoo.com",
+    password: "marymary",
+    userName: "Mary",
+    firstName: "Mary",
+    lastName: "Mary",
+  },
+];
+
+module.exports.users = users;
