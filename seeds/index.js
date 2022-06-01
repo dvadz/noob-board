31cfd48f2e5aa374a4bd5d7c89ff8bec7a5a239f
Daniel Vadil <dvadz@yahoo.com>
2022-06-01 12:50:49 -0400

    Cleanup, removed remaining test code

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,6 @@
-const dotenv = require("dotenv");
+require("dotenv").config({ path: "../.env" });
 const User = require("../models/User");
 const mongoose = require("mongoose");
-
-const result = dotenv.config({ path: "../.env" });
-const Db = process.env.MONGODB_URI;
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("error", console.error.bind(console, "Noob database connection error during seeding"));
